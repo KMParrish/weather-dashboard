f65c1adae6799f7601bb9876c24bafbea1b39692
Kyle Parrish <110362534+KMParrish@users.noreply.github.com>
2022-09-22 23:14:01 -0500

    added more js functionality and promise

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,5 @@
-function initPage() {
-    const cityEl = document.getElementById("enter-city");
-    const searchEl = document.getElementById("search-button");
-    const clearEl = document.getElementById("clear-history");
-    const nameEl = document.getElementById("city-name");
-    const currentPicEl = document.getElementById("current-pic");
-    const currentTempEl = document.getElementById("temperature");
-    const currentHumidityEl = document.getElementById("humidity");
-    const currentWindEl = document.getElementById("wind-speed");
-    const currentUVEl = document.getElementById("UV-index");
-    const historyEl = document.getElementById("history");
-    var fivedayEl = document.getElementById("fiveday-header");
-    var todayweatherEl = document.getElementById("today-weather");
-    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-}
-let city;
-let url;
+let searchHistory = []
+const historyEl = document.getElementById("history");
 const search = document.getElementById("search-button")
 const APIKey = "981ead18d7328e5f3ebe8d665a0eeefd"
 var requestOptions = {
@@ -22,14 +7,121 @@ var requestOptions = {
     redirect: 'follow'
   };
   
-  fetch("https://api.openweathermap.org/data/2.5/forecast?q=San Antonio&units=imperial&appid=981ead18d7328e5f3ebe8d665a0eeefd", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error)); 
+  function getWindSpeed(data) {
+    return data["wind"]["speed"]
+  }
+  function getWeatherMain(data) { 
+    return data["weather"][0]["main"]
+  }
+  function getHumidity(data){
+    return data["main"]["humidity"]
+  }
+  function getTemp(data){
+    return data["main"]["temp"]
+  }
+  function getDate(data) {
+    var dateTime = data["dt_txt"]
+    return dateTime.split(" ")[0]
+  }
+  async function fiveDaysApi(urlRequest) {
+    const response = await fetch(urlRequest)
+        .then(function (data) {
+            return data.json();
+        }).then(function (data) {
+            console.log(data);
+            const windSpeeds = []
+            const weatherMains = []
+            const humidities = []
+            const temps = []
+            const dates = []
+            const recordCount = data["cnt"]
+            for (let i = 0; i < recordCount; i += 1) {
+                let record = data.list[i];
+                windSpeeds.push(getWindSpeed(record))
+                weatherMains.push(getWeatherMain(record))
+                humidities.push(getHumidity(record))
+                temps.push(getTemp(record))
+                dates.push(getDate(record))
+            }
+            var days = propogateData(dates, windSpeeds, weatherMains, humidities, temps)
+            return days
+        })
+    return await response
+}
+function propogateData(dates, windSpeeds, weatherMains, humidities, temps) {
+    days = {}
+    for (let i = 0; i < dates.length; i ++) {
+        let currentDate = dates[i]
+        let currentWindSpeed = windSpeeds[i]
+        let currentWeatherMain= weatherMains[i]
+        let currentHumidity = humidities[i]
+        let currentTemp = temps[i]
+        
+        //if current date does not exist in days
+        if (!(currentDate in days)){
+            days[currentDate] = {"windSpeeds" : [currentWindSpeed],
+                                 "weatherMains" :[currentWeatherMain],
+                                 "humidities" : [currentHumidity],
+                                 "temps" : [currentTemp]
+                                }
 
-  search.addEventListener("click", function () {
-    city = document.getElementById("enter-city").value
-    url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=981ead18d7328e5f3ebe8d665a0eeefd`
+            
+        }
+        else {
+            //if current date does exist in days
+            days[currentDate]["windSpeeds"].push(currentWindSpeed)
+            days[currentDate]["weatherMains"].push(currentWeatherMain)
+            days[currentDate]["humidities"].push(currentHumidity)
+            days[currentDate]["temps"].push(currentTemp)
+        }
+    }
+    days = calculateAverages(days)
+    return days
+}
+function getAverage(arr) {
+    let sum = 0
+    for (let i = 0; i < arr.length; i ++){
+        sum += arr[i]
+    }
+    let average = sum / arr.length
+    return average.toFixed(2)
+}
+function calculateAverages(days) {
+    for(let date in days){
+       days[date]["temps"] = getAverage(days[date]["temps"])
+       days[date]["windSpeeds"] = getAverage(days[date]["windSpeeds"])
+       days[date]["humidities"] = getAverage(days[date]["humidities"])
+       days[date]["weatherMains"] = days[date]["weatherMains"][0]
+    }
+    return days
+}
+
+search.addEventListener("click", function () {
+    function renderSearchHistory() {
+        historyEl.innerHTML = "";
+        for (let i = 0; i < searchHistory.length; i++) {
+            const historyItem = document.createElement("input");
+            historyItem.setAttribute("type", "text");
+            historyItem.setAttribute("readonly", true);
+            historyItem.setAttribute("class", "form-control d-block bg-white");
+            historyItem.setAttribute("value", searchHistory[i]);
+            historyItem.addEventListener("click", function () {
+                fiveDaysApi(historyItem.value);
+            })
+            historyEl.append(historyItem);
+        }
+    }
+    function renderCards(data) {
+        for (let i = 1; i <= data.length; i++) {
+            const outerDiv = document.createElement("div")
+            outerDiv.id = `day-${i}`
+        }
+    }
+    const city = document.getElementById("enter-city").value
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=981ead18d7328e5f3ebe8d665a0eeefd`
     console.log(city)
-  })
-  
+    fiveDaysApi(url).then((data)=>{
+        
+    })
+    console.log(data)
+})
